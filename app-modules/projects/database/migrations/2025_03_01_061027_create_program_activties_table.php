5a49c8af051c100ca7fc9ee79689ce47338376fe
Kali <kali@kali.kali>
2025-03-01 01:19:55 -0500

    first frontedn

--- app-modules/projects/database/migrations/2025_03_01_061027_create_program_activties_table.php
+++ app-modules/projects/database/migrations/2025_03_01_061027_create_program_activties_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('program_statuses', function (Blueprint $table) {
+        Schema::create('program_activties', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('color')->default('#cecece');
-            $table->boolean('is_default')->default(false);
-            $table->tinyInteger('order');
+            $table->foreignId('program_id');
+            $table->foreignId('old_status_id');
+            $table->foreignId('new_status_id');
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('program_statuses');
+        Schema::dropIfExists('program_activties');
     }
 };
