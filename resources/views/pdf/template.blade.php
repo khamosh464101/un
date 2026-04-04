4389c39f8d03014633de0dc801db15c56359bbaa
khamosh464101 <azim@momtaz.af>
2026-04-04 11:30:53 +0430

    4 aprdil 2025

--- resources/views/pdf/template.blade.php
+++ resources/views/pdf/template.blade.php
@@ -314,13 +314,8 @@
         </td>
     </tr>
     <tr>
-        <td style="height:250px; " class="center">
-            @php
-            $document = str_replace('http://127.0.0.1:8000/', '', $submission->houseLandOwnership?->landOwnershipDocument?->first()?->house_document_photo);
-            @endphp
-            <img src="{{ $submission->houseLandOwnership?->landOwnershipDocument?->first()?->house_document_photo ?? asset('images/default.png') }}" alt="تصویر" style="max-width:100%; max-height:250px;">
+        
 
-        </td>
         <td style="height:250px;" class="center">
             @php
                 $map = str_replace('http://127.0.0.1:8000/', '', $location['map_image']);
